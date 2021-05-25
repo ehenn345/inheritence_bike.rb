5fb3ef48e94dc4639c5eb49dd7245e577138cdd0
Erin <westonerin1@gmail.com>
2021-05-24 21:45:09 -0400

    add raise instance method and writer/setter method and getter/reader method

--- ruby_oop.rb
+++ ruby_oop.rb
@@ -7,13 +7,30 @@ class Employee
   end
 
   def print_info
-    p "#{@first_name} #{@last_name} makes #{@salary} per year "
+    p "#{@first_name} #{@last_name} makes #{@salary.round} per year "
   end
 
+  def give_annual_raise
+    @salary = @salary * 1.05
+  end
+
+  def first_name  #getter/reader method
+    @first_name
+  end
+
+  def first_name=(input_name) #writer/setter method
+    @first_name = input_name
+  end
 end
 
-employee1 = Employee.new("Tito", "Burrito", "80000", "active")
-employee1.print_info
+employee1 = Employee.new("Tito", "Burrito", 80000, true)
+p employee1.give_annual_raise
+p employee1.print_info
+employee1.first_name=("Pooh")
+p employee1.first_name
+p employee1.print_info
 
-employee2 = Employee.new("Jim", "Joe", "60000", "active")
+employee2 = Employee.new("Jim", "Joe", 60000, false)
+employee2.print_info
+employee2.give_annual_raise
 employee2.print_info
